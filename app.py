61993a07085dd39c02d4736961a392c69969e854
Bose <bosem3708@gmail.com>
2025-06-13 13:42:03 +0530

    Fixed requirements.txt for Render

--- app.py
+++ app.py
@@ -47,8 +47,7 @@ def search():
     return render_template('index.html', employees=results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True,host="0.0.0.0", port=5000)
     
 
  
